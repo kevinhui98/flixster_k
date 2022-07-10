95de0ed32bf8749f6cc96a2537237c577a245cd3
Kevin Hui <kevinhui98@gmail.com>
2022-07-09 21:55:48 -0400

    complete flixter part 1

--- app/src/main/java/com/example/flixster_k/MainActivity.kt
+++ app/src/main/java/com/example/flixster_k/MainActivity.kt
@@ -3,6 +3,8 @@ package com.example.flixster_k
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.codepath.asynchttpclient.AsyncHttpClient
 import com.codepath.asynchttpclient.callback.JsonHttpResponseHandler
 import okhttp3.Headers
@@ -12,9 +14,22 @@ private const val TAG = "MainActivity"
 private const val NOW_PLAYING_URL = "https://api.themoviedb.org/3/movie/now_playing?api_key=a07e22bc18f5cb106bfe4cc1f83ad8ed"
 class MainActivity : AppCompatActivity() {
     private val movies = mutableListOf<Movie>()
+    private lateinit var rvMovies : RecyclerView
+
+    //Define a model class to use as the data source - DONE
+    //Add a RecyclerView to your activity to display the items - DONE
+    //Create a custom row layout XML file to visualize the item - DONE
+    //Create a RecyclerView.Adapter and ViewHolder to render the item - DONE
+    //Bind the adapter to the data source to populate the RecyclerView - DONE
+    //Bind a layout manager to the RecyclerView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        rvMovies = findViewById(R.id.rvMovies)
+        val movieAdapter = MovieAdapter(this, movies)
+        rvMovies.adapter = movieAdapter
+        rvMovies.layoutManager = LinearLayoutManager(this)
 
         val client = AsyncHttpClient()
         client.get(NOW_PLAYING_URL,object: JsonHttpResponseHandler(){
@@ -29,6 +44,7 @@ class MainActivity : AppCompatActivity() {
                 try {
                     val movieJSONArray = json.jsonObject.getJSONArray("results")
                     movies.addAll(Movie.fromJsonArray(movieJSONArray))
+                    movieAdapter.notifyDataSetChanged()
                     Log.i(TAG, "Movie list $movies")
                 } catch (e: JSONException) {
                     Log.e(TAG, "Encoutered exception $e")
